686966f9e2a2350ddced3095622e044a01286e43
Niloufar Ghadiri <95937588+Niloufar-Ghadiri@users.noreply.github.com>
2022-02-26 18:22:25 +0330

    Add files via upload

---
+++ mean.py
@@ -0,0 +1,12 @@
+full_name=(input("please enter your first and surname: "))
+Lesson1=float(input())
+Lesson2=float(input())
+Lesson3=float(input())
+sum1=Lesson1+Lesson2+Lesson3
+x=sum1/3
+if x<12:
+ print("Fail")
+elif 12<=x<17:
+ print("Normal")
+else :
+ print("Great")
